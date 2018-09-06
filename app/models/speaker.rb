4efe6858a0c345880fc044443d0aa8d2a29912c2
byrne-saw <byrnes.aw@gmail.com>
2018-09-06 17:47:53 -0500

    day 14 hw

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,6 +1,6 @@
 class Speaker < ApplicationRecord
-  validate :first_name, length: {minimum: 2 }
-  validate :last_name, length: {minimum: 2 }
-  validate :email, uniqueness: true
-  validate :age, numericality: { greater_than_or_equal_to: 18 }
+  validates :first_name, length: {minimum: 2 }
+  validates :last_name, length: {minimum: 2 }
+  validates :email, uniqueness: true
+  validates :age, numericality: { greater_than_or_equal_to: 18 }
 end
