773c15a072d2ea4f126476fa085eefa108b75035
byrne-saw <byrnes.aw@gmail.com>
2018-09-10 18:20:06 -0500

    finishing day 14 hw - more exercises

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,6 @@
 class Meeting < ApplicationRecord
+  validates :title, presence: true
+  validates :agenda, presence: true
+  validates :time, presence: true
+  validates :location, presence: true
 end
